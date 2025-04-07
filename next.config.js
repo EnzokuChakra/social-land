e9a1ddbd6a5ef58b6352da80918c4f2fa37b180b
Your Name <user@example.com>
2025-04-07 21:41:58 +0300

    Fix: X-Headers v2

--- next.config.js
+++ next.config.js
@@ -100,7 +100,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors *; default-src 'self' 'unsafe-inline' 'unsafe-eval' https: data: blob:;"
+            value: "frame-ancestors *; default-src 'self' 'unsafe-inline' 'unsafe-eval' https: data: blob:; connect-src 'self' ws: wss: localhost:5002 ws://localhost:5002 wss://localhost:5002;"
           },
           {
             key: 'Access-Control-Allow-Origin',
