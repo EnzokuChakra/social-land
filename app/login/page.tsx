3d589fc72e862fedf314fc2170d852b85499a81e
Your Name <user@example.com>
2025-04-07 08:01:00 +0300

    add password reset revert

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -170,14 +170,6 @@ export default function LoginPage() {
                       />
                     </FormControl>
                     <FormMessage />
-                    <div className="text-sm">
-                      <Link
-                        href="/reset-password"
-                        className="text-sky-500 hover:text-sky-400 font-medium"
-                      >
-                        Forgot password? Reset now
-                      </Link>
-                    </div>
                   </FormItem>
                 )}
               />
